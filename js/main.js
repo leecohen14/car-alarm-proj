c3bff6b094213db892dc98ecf72e398ef2bc8d8f
leecohen14 <74984690+leecohen14@users.noreply.github.com>
2021-06-01 12:22:30 +0300

    second update

--- js/main.js
+++ js/main.js
@@ -2,16 +2,16 @@
 console.log('work');
 
 function onAccident() {
-    var sound = new Audio('../sounds/car accident.mp3');
+    var sound = new Audio('sounds/car accident.mp3');
     sound.play();
 }
 
 function onHorn() {
-    var sound = new Audio('../sounds/horn1.mp3');
+    var sound = new Audio('sounds/horn1.mp3');
     sound.play();
 }
 
 function onHitBody() {
-    var sound = new Audio('../sounds/hit body.mp3');
+    var sound = new Audio('sounds/hit body.mp3');
     sound.play();
 }
