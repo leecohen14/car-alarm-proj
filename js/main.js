83fedf2f15eca0d2c43cc23e58dc891faed2d7a4
leecohen14 <74984690+leecohen14@users.noreply.github.com>
2021-06-01 12:14:07 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+'use strict'
+console.log('work');
+
+function onAccident() {
+    var sound = new Audio('../sounds/car accident.mp3');
+    sound.play();
+}
+
+function onHorn() {
+    var sound = new Audio('../sounds/horn1.mp3');
+    sound.play();
+}
+
+function onHitBody() {
+    var sound = new Audio('../sounds/hit body.mp3');
+    sound.play();
+}
